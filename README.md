7169f31458da77ec0216d37ea31c1040ce69afa2
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-02-29 00:51:21 +0100

    Feat: Small mobil menu animation

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# APP NAME ( Other content )
-## The Odin Project
-A simple desciption
+# Dynamic User Interface Interactions
+## JavaScript Course ( The Odin Project )
+A simple animated mobile menu.
 
 Links:
 
-:white_check_mark: [Assigments](!#) :blue_book:
+:white_check_mark: [Assigments](https://www.theodinproject.com/lessons/node-path-javascript-dynamic-user-interface-interactions) :blue_book:
 
-:white_check_mark: [Live Preview](!#) :computer:
-
-:white_check_mark: [Code](!#) :eight_spoked_asterisk:
+:white_check_mark: [Live Preview](https://carlosfrontend.github.io/mobile-menu-odin-project/) :computer:
 
  Happy Coding!! :coffee: :rocket:
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './styles/main.css';
 
-console.log('Hello Starter Template!');
+const navbar = document.querySelector('#navbar');
+const menuButton = document.querySelector('#menu-button');
+const otherButtons = [...navbar.children].filter((el) => (el.id !== 'menu-button' ? el : null));
+const homeButton = document.querySelector('#home-button');
+const profileButton = document.querySelector('#profile-button');
+const sendButton = document.querySelector('#send-button');
+const settingsButton = document.querySelector('#settings-button');
+
+const toggleMenuAnimation = (e) => {
+  if (e.target.closest('#menu-button')) {
+    otherButtons.forEach((btn) => btn.classList.toggle('showButtons'));
+    navbar.classList.toggle('showNavbar');
+    homeButton.classList.toggle('rotateLeft');
+    profileButton.classList.toggle('rotateLeft');
+    sendButton.classList.toggle('rotateRight');
+    settingsButton.classList.toggle('rotateRight');
+  }
+  // eslint-disable-next-line no-shadow
+  const handleFocus = (e) => {
+    e.currentTarget.firstChild.classList.add('changeIconsStyles');
+  };
+
+  // eslint-disable-next-line no-shadow
+  const handleBlur = (e) => {
+    e.currentTarget.firstChild.classList.remove('changeIconsStyles');
+  };
+  otherButtons.forEach((el) => el.addEventListener('focus', handleFocus));
+  otherButtons.forEach((el) => el.addEventListener('blur', handleBlur));
+};
+
+const animateMobileMenu = () => {
+  menuButton.addEventListener('click', toggleMenuAnimation);
+};
+
+animateMobileMenu();
